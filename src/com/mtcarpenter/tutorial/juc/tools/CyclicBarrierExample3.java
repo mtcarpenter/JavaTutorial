7c84de94aa141bc5f7f34d0776514c7d497d71db
mtcarpenter <dreamlixc@163.com>
2020-09-29 10:14:46 +0800

    突击并发编程JUC系列-并发工具 CyclicBarrier

---
+++ src/com/mtcarpenter/tutorial/juc/tools/CyclicBarrierExample3.java
@@ -0,0 +1,46 @@
+package com.mtcarpenter.tutorial.juc.tools;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author mtcarpenter
+ * @github https://github.com/mtcarpenter/JavaTutorial
+ * @info 微信公众号：山间木匠
+ */
+public class CyclicBarrierExample3 {
+
+    private final static int gradeNum = 6;
+
+    private static CyclicBarrier barrier = new CyclicBarrier(3, new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("******所有子线程达到屏障******");
+        }
+    });
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newScheduledThreadPool(gradeNum);
+        System.out.println("通知、通知，请准备的年级先出发.....");
+        for (int i = 0; i < gradeNum; i++) {
+            TimeUnit.SECONDS.sleep(1);
+            int gradeName = i + 1;
+            exec.submit(() -> {
+                try {
+                    wait(gradeName);
+                } catch (Exception e) {
+                }
+            });
+        }
+        exec.shutdown();
+    }
+
+    private static void wait(int gradeName) throws Exception {
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(gradeName + "年级所有同学来到了出口......");
+        barrier.await();
+        System.out.println(gradeName + "年级所有同学到出发");
+    }
+}
